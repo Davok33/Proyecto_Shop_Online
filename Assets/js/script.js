7a2ad302b6291ddadf494f1b793e6c92c33e0e7e
Davidcc <deibycc289@gmail.com>
2022-06-13 22:12:00 +0200

    c3

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,34 @@
+
 import basedeDatos from "./bdatos.js";
 
 const bdatos = JSON.parse(basedeDatos);
+
+var url = new URL(window.location);
+var params = new URLSearchParams(url.search);
+let ropaParams = params.get("genero")
+
+function filtrado(genero){
+    let filtbd = bdatos.filter(ropa => ropa.genero.includes(genero))
+    return filtbd;
+
+}
+let filtradd = filtrado(ropaParams);
+console.log(filtradd);
+
 function ropahmn (articulo, imagen){
     let ropa1= '<div class="ropa">';
-    ropa1 +=`<img src='${imagen}'><br>`;
+    ropa1 +=`<img src='${imagen}'><br>`; 
     ropa1 += `<span> Articulo: ${articulo} </span><br>`; 
     ropa1 += '</div>';
     return ropa1;
 }
 let ropa_hmn = "";
 
-for (let i=0; i<10; i++){ 
-    let imagen = bdatos[i].imagen;
-    let articulo = bdatos[i].articulo;
-    ropa_hmn += ropahmn(imagen,  articulo)
+for (let i=0; i<filtradd; i++){ 
+    let imagen1 = filtradd[i].imagen; 
+    let articulo1 = filtradd[i].articulo;
+    ropa_hmn += ropahmn (articulo1, imagen1)
 }
 document.querySelector('.cajaimg').innerHTML = ropa_hmn
-console.log(ropa_hmn);
+
+
