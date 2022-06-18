75f6cf373addfa56c1ff88eed93e3668a491a941
Davidcc <deibycc289@gmail.com>
2022-06-18 21:22:39 +0200

    cambios finales 1

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,9 +15,10 @@ function filtrado(genero){
 let filtramujer = filtrado(ropaParams);
 console.log(filtramujer);
 
-function ropahmn (articulo, imagen,){      //
+function ropahmn (articulo, imagen, talla){      //
     let ropa1 = '<div class="ropa">';
     ropa1 +=`<img src='${imagen}'><br>`;
+    ropa1 += `<span> Talla: ${talla} </span><br>`;
    /*  ropa1 += `<span> Género: ${genero} </span><br>`;  */
     ropa1 += `<span> Articulo: ${articulo} </span><br>`; 
     ropa1 += '</div>';
@@ -27,9 +28,9 @@ let ropa_hmn = "";
 
 for (let i = 0; i < filtramujer.length; i++){ 
     let imagen1 = filtramujer[i].imagen; 
-    /* let genero1 = filtradd [i].genero; */
+    let talla1 = filtramujer [i].talla; 
     let articulo1 = filtramujer[i].articulo;
-    ropa_hmn += ropahmn (articulo1, imagen1)
+    ropa_hmn += ropahmn (articulo1, imagen1, talla1)
 }
 document.querySelector('.cajaimg').innerHTML = ropa_hmn
 
